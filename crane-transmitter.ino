3d67c79bd5a5748e4b1611e5d5d6bc1cab573efe
Bello Shehu <belloshehu1@gmail.com>
2025-10-19 09:59:28 +0100

    Finish all commands for operating the crane remotely

--- crane-transmitter.ino
+++ crane-transmitter.ino
@@ -26,8 +26,10 @@ Date: 30/07/2025
 #define BTN_FORWARD_PIN 4 // sw2
 #define BTN_RIGHT_PIN 5 // sw3
 #define BTN_BACKWARD_PIN 6 // sw4
-#define BTN_HOOK_UP_PIN 7 // sw5
-#define BTN_HOOK_DOWN_PIN 8 // sw6
+#define BTN_HOOK_UP_PIN 7 // sw5: button for controlling hook downward
+#define BTN_HOOK_DOWN_PIN 8 // sw6: button for controlling the hook upward
+#define BTN_ROTATE_LEFT_PIN 19 // sw5: button for controlling rotation left
+#define BTN_ROTATE_RIGHT_PIN 17 // sw6: button for controlling the rotation right
 
 // --- NRF MODULE ---
 #define NRF_MISO_PIN 12
@@ -38,7 +40,7 @@ Date: 30/07/2025
 
 // --- LED PINS
 #define LED_SIGNAL 18
-#define LED_POWER 19
+// #define LED_POWER 19
 
 // --- VOLTAGE SEBNSOR
 #define VOLT_SENSOR_PIN A3
@@ -65,11 +67,12 @@ int adc_value = 0;
          
 */
 
-#define LEFT_THRESHOLD 400 // anything less 400 is condsidered left
-#define RIGHT_THRESHOLD 540 // anything 500 or higher is considered right
+#define LEFT_THRESHOLD 450 // anything less 450 is condsidered left
+#define RIGHT_THRESHOLD 600 // anything 600 or higher is considered right
 #define UP_THRESHOLD 400 // anything 400 or more
-#define DOWN_THRESHOLD 300 // anything 300 or less is down
-
+#define DOWN_THRESHOLD 330 // anything 300 or less is down
+const int xAxisNeutral = 310;
+const int yAxisNeutral = 460;
 // BUTTONS
 ezButton btnLeft(BTN_LEFT_PIN);
 ezButton btnForward(BTN_FORWARD_PIN);
@@ -77,28 +80,23 @@ ezButton btnRight(BTN_RIGHT_PIN);
 ezButton btnBackward(BTN_BACKWARD_PIN);
 ezButton btnHookUp(BTN_HOOK_UP_PIN);
 ezButton btnHookDown(BTN_HOOK_DOWN_PIN);
+ezButton btnRotateLeft(BTN_ROTATE_LEFT_PIN);
+ezButton btnRotateRight(BTN_ROTATE_RIGHT_PIN);
 
 RF24 radio(NRF_CE_PIN, NRF_CSN_PIN);
 const byte address[6] = "00011";
 
+unsigned int val[2] ;
 unsigned long lastTimeVoltageReading = 0;
 byte ledState = LOW;
 
-struct Data_package {
-  byte left = 0;         // turns the vehicle to left
-  byte right = 0;        // turns the vehicle to right
-  byte forward = 1;      // moves the whole crane forward
-  byte backward = 0;     // moves the whole crane backward
-  byte up = 1;           // moves the crane up
-  byte down = 0;         // moves the crane down
-  byte rotateLeft = 0;   // rotates the crane right to left
-  byte rotateRight = 1;  // rotates the crane left to right
-  byte hookUp = 0;       // moves the hoot up
-  byte hookDown = 0;     // moves the hook down
+struct JoystickValues {
+  int x = 0;
+  int y = 0;
 };
 
 int boomYValue, boomXValue = 0;
-Data_package data;  // Create a variable with the above structure
+byte data = 0;  // Create a variable with the above structure
 void toggleSignalLED(byte state = LOW);
 
 void setup() {
@@ -106,9 +104,10 @@ void setup() {
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
-  // Serial.begin(9600);
+
+  //Serial.begin(9600);
   pinMode(LED_SIGNAL, OUTPUT);
-  pinMode(LED_POWER, OUTPUT);
+  //pinMode(LED_POWER, OUTPUT);
 
   digitalWrite(LED_SIGNAL, HIGH);
   digitalWrite(LED_SIGNAL, HIGH);
@@ -119,196 +118,95 @@ void setup() {
 }
 
 void loop() {
-  // listen button presss
-  btnLeft.loop();
-  btnForward.loop();
-  btnRight.loop();
-  btnBackward.loop();
-  btnHookUp.loop();
-  btnHookDown.loop();
-  
-  if(btnLeft.isPressed()){
-    data.up = 0;
-    data.down = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 0;
-    data.left = 1;
-    data.right = 0;
-    data.backward = 0;
-    data.forward = 0;
-    data.hookUp = 0;
-    data.hookDown = 0;
-    toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
-
-  }
-  if(btnRight.isPressed()){
-    data.up = 0;
-    data.down = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 0;
-    data.right = 1;
-    data.left = 0;
-    data.backward = 0;
-    data.forward = 0;
-    data.hookUp = 0;
-    data.hookDown = 0;
-    toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
-
-  }
-  if(btnForward.isPressed()){
-    data.up = 0;
-    data.down = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 0;
-    data.forward = 1;
-    data.left = 0;
-    data.right = 0;
-    data.backward = 0;
-    data.hookUp = 0;
-    data.hookDown = 0;
-    toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
-
-  }
-  if(btnBackward.isPressed()){
-    data.up = 0;
-    data.down = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 0;
-    data.backward = 1;
-    data.left = 0;
-    data.right = 0;
-    data.forward = 0;
-    data.hookUp = 0;
-    data.hookDown = 0;
-    toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
-
-  }
-  if(btnHookUp.isPressed()){
-    data.up = 0;
-    data.down = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 0;
-    data.hookUp = 1;
-    data.hookDown = 0;
-    data.backward = 0;
-    data.left = 0;
-    data.right = 0;
-    data.forward = 0;
-    radio.write(&data, sizeof(Data_package));
-  }
-  if(btnHookDown.isPressed()){
-    data.up = 0;
-    data.down = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 0;
-    data.hookDown = 1;
-    data.hookUp = 0;
-    data.backward = 0;
-    data.left = 0;
-    data.right = 0;
-    data.forward = 0;
-    toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
-  }
-
-  boomYValue = analogRead(AUX_VRX_PIN);
-  boomXValue = analogRead(AUX_VRY_PIN);
+  JoystickValues val = getAverage();
+  boomXValue = analogRead(AUX_VRX_PIN);
+  boomYValue = analogRead(AUX_VRY_PIN);
+;
   // Serial.println("yValue:"+ String(boomYValue));
   // Serial.println("xValue:"+ String(boomXValue));
-
-  // check left/right press
-  if (boomYValue >= UP_THRESHOLD) {
-   // Serial.println("Boom up");
-    data.up = 1;
-    data.down = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 0;
-    data.hookDown = 0;
-    data.hookUp = 0;
-    data.backward = 0;
-    data.left = 0;
-    data.right = 0;
-    data.forward = 0;
+  
+  if(digitalRead(BTN_LEFT_PIN)== LOW){
+    data = 4;
     toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
-
-  } else if (boomYValue <= DOWN_THRESHOLD) {
-   //Serial.println("boom down ");
-    data.down = 1;
-    data.up = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 0;
-    data.hookDown = 0;
-    data.hookUp = 0;
-    data.backward = 0;
-    data.left = 0;
-    data.right = 0;
-    data.forward = 0;
+    radio.write(&data, sizeof(data));
+    //Serial.println("drive left");
+  }
+  else if(digitalRead(BTN_RIGHT_PIN)== LOW){
+    data = 2;
     toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
+    radio.write(&data, sizeof(data));
+    //Serial.println("drive right");
+  }
+  else if(digitalRead(BTN_FORWARD_PIN)== LOW){
+    data = 1;
+    toggleSignalLED(HIGH);
+    radio.write(&data, sizeof(data));
+    //Serial.println("drive forward");
+  }
+  else if(digitalRead(BTN_BACKWARD_PIN)== LOW){
+    data = 3;
+    toggleSignalLED(HIGH);
+     radio.write(&data, sizeof(data));
+    //Serial.println("drive back");
+
+  }
+  else if(digitalRead(BTN_HOOK_UP_PIN)== LOW){
+    data = 7;
+    radio.write(&data, sizeof(data));
+   //Serial.println("hook up");
+  }
+  else if(digitalRead(BTN_HOOK_DOWN_PIN)== LOW){
+    data = 8;
+    toggleSignalLED(HIGH);
+    radio.write(&data, sizeof(data));
+    //Serial.println("hook down");
   }
 
-  // check up/donw press
-  if (boomXValue <= LEFT_THRESHOLD) {
+  // check left/right joystick  presss to control boom 
+  else if ((boomYValue > 600 && boomXValue >= 410)) {
+   // boom up
+    data = 9;
+    toggleSignalLED(HIGH);
+    radio.write(&data, sizeof(data));
+  } else if ((boomYValue < 360 && boomXValue <= 240)) {
+   // boom down
+    data = 10;
+    toggleSignalLED(HIGH);
+    radio.write(&data, sizeof(data));
+  }
+
+  // check up/donw press to rotate left right
+  else if (digitalRead(BTN_ROTATE_RIGHT_PIN)== LOW) {
    //Serial.println("boom left");
-    data.down = 0;
-    data.up = 0;
-    data.rotateLeft = 1;
-    data.rotateRight = 0;
-    data.hookDown = 0;
-    data.hookUp = 0;
-    data.backward = 0;
-    data.left = 0;
-    data.right = 0;
-    data.forward = 0;
+    data = 11;
     toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
-  } else if (boomXValue >= RIGHT_THRESHOLD) {
+    radio.write(&data, sizeof(data));
+    //Serial.println("boom left");
+  } else if (digitalRead(BTN_ROTATE_LEFT_PIN)== LOW ) {
    // Serial.println("boom right");
-    data.down = 0;
-    data.up = 0;
-    data.rotateLeft = 0;
-    data.rotateRight = 1;
-    data.hookDown = 0;
-    data.hookUp = 0;
-    data.backward = 0;
-    data.left = 0;
-    data.right = 0;
-    data.forward = 0;
+    data = 12;
     toggleSignalLED(HIGH);
-    radio.write(&data, sizeof(Data_package));
+    radio.write(&data, sizeof(data));
+   // Serial.println("boom right");
+  }else{
+    data = 255;
+    radio.write(&data, sizeof(data));
   }
-
   // Send the whole data from the structure to the receiver
   // radio.write(&data, sizeof(Data_package));
-
-  // reset all values:
-  data.down = 0;
-  data.up = 0;
-  data.rotateLeft = 0;
-  data.rotateRight = 0;
-  data.hookDown = 0;
-  data.hookUp = 0;
-  data.backward = 0;
-  data.left = 0;
-  data.right = 0;
-  data.forward = 0;
+  // radio.write(&data, sizeof(data));
   toggleSignalLED(LOW);
   scanBatteryVoltage();
-  delay(1);
+  delay(10);
 }
 
 void toggleSignalLED(byte state=LOW){
   digitalWrite(LED_SIGNAL, state);
 }
 
-void togglePowerLED( byte state=LOW){
-  digitalWrite(LED_POWER, state);
-}
+// void togglePowerLED( byte state=LOW){
+//   digitalWrite(LED_POWER, state);
+// }
 
 float getVoltage(){
   float adc_value = analogRead(VOLT_SENSOR_PIN);
@@ -325,16 +223,32 @@ void scanBatteryVoltage(){
     if(volt < 10){
       // turn blink RED LED when less 10v
       if(ledState){
-        togglePowerLED(LOW);
+        //togglePowerLED(LOW);
       }else{
-        togglePowerLED(HIGH);
+        //togglePowerLED(HIGH);
       }
       ledState = !ledState;
     }else {
       // turn LED 
-      togglePowerLED(HIGH);
+      //togglePowerLED(HIGH);
     }
     lastTimeVoltageReading = millis();
    //Serial.println(volt);
   }
+}
+
+JoystickValues getAverage(){
+  int sumX = 0;
+  int sumY = 0;
+  JoystickValues xyValues;
+  xyValues.x = 0;
+  xyValues.y = 0;
+  for(int i = 0; i < 5; i++){
+     sumX = sumX + analogRead(AUX_VRX_PIN);
+     sumY = sumY + analogRead(AUX_VRY_PIN);
+     delay(10);
+  }
+  xyValues.x = sumX / 10;
+  xyValues.y = sumY / 10;
+  return xyValues;
 }
